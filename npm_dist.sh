82585511aa10b42a70fed7deb8253c762abb1b10
matt <matt@videa.ai>
2023-09-28 17:26:01 -0500

    ship as cjs

--- npm_dist.sh
+++ npm_dist.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-#rm -rf ./dist
-#mkdir ./dist
+
+# Create the necessary folders for the distribution
+# Note: The original script had commands to remove the dist folder; this has been commented out
+# Uncomment if you need to clear out the dist folder each time
+# rm -rf ./dist
+# mkdir ./dist
 mkdir ./dist/dist
+
+# Move the built jsts files to the dist/dist directory
 mv ./dist/jsts.* ./dist/dist
+
+# Copy necessary source files and directories to the dist folder
 cp -r ./src/java ./dist/java
 cp -r ./src/org ./dist/org
 cp ./src/hasInterface.js ./dist/hasInterface.js
+
+# Copy package metadata and documentation to the dist folder
 cp ./package.json ./dist/package.json
 cp ./README.md ./dist/README.md
+
+# Remove any .ast files from the dist folder
 find ./dist -name '*.ast' -type f -delete
+
+# npm publish
+cd ./dist
+npm publish --access public
--- rollup.config.js
+++ rollup.config.js
@@ -6,30 +6,57 @@ import terser from '@rollup/plugin-terser'
 const packageJson = JSON.parse(fs.readFileSync('./package.json'))
 const license = fs.readFileSync('./license.txt', { encoding: 'utf8' })
 
-export default {
-  input: 'src/jsts.js',
-  output: {
-    file: 'dist/jsts.js',
-    format: 'umd',
-    name: 'jsts',
-    banner: license,
-    sourcemap: true
+export default [
+  {
+    input: 'src/jsts.js',
+    output: {
+      file: 'dist/jsts.js',
+      format: 'umd',
+      name: 'jsts',
+      banner: license,
+      sourcemap: true,
+    },
+    plugins: [
+      replace({
+        npm_package_version: packageJson.version,
+        preventAssignment: true,
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        presets: [
+          [
+            '@babel/env',
+            {
+              targets: {
+                browsers: ['>2%', 'not dead'],
+              },
+            },
+          ],
+        ],
+        babelrc: false,
+        babelHelpers: 'bundled',
+      }),
+      terser(),
+    ],
   },
-  plugins: [
-    replace({
-      npm_package_version: packageJson.version,
-      preventAssignment: true
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      presets: [['@babel/env', {
-        targets: {
-          browsers: ['>2%', 'not dead']
-        }
-      }]],
-      babelrc: false,
-      babelHelpers: 'bundled'
-    }),
-    terser()
-  ]
-}
+  // Added configuration for CommonJS output
+  {
+    input: 'src/jsts.js',
+    output: {
+      file: 'dist/jsts.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [
+      replace({
+        npm_package_version: packageJson.version,
+        preventAssignment: true,
+      }),
+      babel({
+        exclude: 'node_modules/**',
+        babelrc: false,
+        babelHelpers: 'bundled',
+      }),
+    ],
+  },
+]
